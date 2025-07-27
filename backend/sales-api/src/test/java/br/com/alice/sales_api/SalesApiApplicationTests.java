dc02109168f1dc4cb1dcd41118d2268e584f5635
alice <alicemarinhogms@gmail.com>
2025-07-27 16:54:24 -0300

    feat: add backend

---
+++ backend/sales-api/src/test/java/br/com/alice/sales_api/SalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alice.sales_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
